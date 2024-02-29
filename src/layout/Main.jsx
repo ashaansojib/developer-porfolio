52786b006ed104a4b6772f37c8b801451f3a22d8
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-02-29 21:43:30 +0600

    full responsive

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../shared/Sidebar';
+import { FaBars, FaX } from 'react-icons/fa6';
 
 const Main = () => {
+    const [showMenu, setShowMenu] = useState(true)
     return (
         <div className='my-container md:my-2'>
+            <div className='absolute top-2 right-0 p-2 block md:hidden'>
+                {
+                    showMenu ? <FaBars onClick={() => setShowMenu(!showMenu)} className='text-2xl cursor-pointer' /> : <FaX onClick={() => setShowMenu(!showMenu)} className='text-2xl cursor-pointer' />
+                }
+            </div>
             <div className='grid md:grid-cols-4 grid-cols-1 justify-between gap-3 '>
-                <aside className='sticky top-0 h-screen hidden md:block'>
+                <aside className={`md:sticky top-0 h-screen w-[300px] ${showMenu ? 'block' : 'hidden'}`}>
                     <Sidebar />
                 </aside>
                 <div className='md:col-span-3 border'>
--- src/shared/Sidebar.jsx
+++ src/shared/Sidebar.jsx
@@ -6,9 +6,9 @@ const Sidebar = () => {
     return (
         <div className='border px-4 py-2'>
             <div className='text-center'>
-                <h2 className='font-semibold pb-2'>Ashaduzzaman Sojib</h2>
+                <h2 className='font-semibold pb-2 md:block hidden'>Ashaduzzaman Sojib</h2>
                 <div className="avatar">
-                    <div className="w-44 rounded-full">
+                    <div className="md:w-44 w-36 rounded-full">
                         <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
                     </div>
                 </div>
